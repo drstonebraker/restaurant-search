bcfc43c477d577bb4cc3d1c8d179a377f55e6c89
drstonebraker <drew.stonebraker@gmail.com>
2017-12-08 13:56:45 -0800

    Revise dataset merge script to remove unneeded data

--- algolia_restaurant_search/src/resources/dataset/merge_script.rb
+++ algolia_restaurant_search/src/resources/dataset/merge_script.rb
@@ -24,9 +24,25 @@ csv_filepath = File.join(File.dirname(__FILE__), 'restaurants_info.csv')
 CSV.foreach(csv_filepath, headers: true, col_sep: ';') do |row|
   id = row['objectID'].to_i
   restaurants_list[id] = restaurants_list[id].merge(row.to_h)
+
+  # delete keys not used in app, for better algolia indexing
+  restaurants_list[id].reject! do |key, val|
+    %w(
+      address
+      city
+      area
+      country
+      phone
+      postal_code
+      price
+      state
+      phone_number
+      dining_style
+    ).include?(key)
+  end
 end
 
 write_filepath = File.join(File.dirname(__FILE__), 'full_restaurants_info.json')
 File.open(write_filepath,"w") do |f|
-  f.write(JSON.pretty_generate(restaurants_list))
+  f.write(JSON.pretty_generate(restaurants_list.values))
 end
